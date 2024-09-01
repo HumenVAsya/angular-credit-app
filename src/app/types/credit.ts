2e24d0cf10565ed5847bc463d5cfd55fa87ebda8
Humen Vasyl <vasyahumenit@gmail.com>
2024-09-02 00:35:52 +0300

    Auto-generated commit

--- src/app/types/credit.ts
+++
@@ -1,9 +0,0 @@
-export interface Credit {
-  "id": number,
-  "user": string,
-  "issuance_date": string,
-  "return_date": string,
-  "actual_return_date": string,
-  "body": number,
-  "percent": number
-}
